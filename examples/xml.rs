6c7bbdb78408fe34a5e10c4bae46a2f31f59a7b6
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2016-07-12 20:17:24 +0200

    added comment

--- examples/xml.rs
+++ examples/xml.rs
@@ -5,7 +5,7 @@ fn main()
     // generate from XML definition
     let raw_code = codespawn::from_xml("examples/sample.xml").unwrap();
     // generate from JSON definition
-    let raw_code = codespawn::from_json("examples/sample.json").unwrap();
+    //let raw_code = codespawn::from_json("examples/sample.json").unwrap();
 
     println!("\n*** Language specific configs (if defined):");
     for c in raw_code.configs.iter() {
