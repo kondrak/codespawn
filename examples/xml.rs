cc64b8971ec107d1fb02a8f26efbeff31cf7d191
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2016-07-12 20:15:23 +0200

    added documented example

--- examples/xml.rs
+++ examples/xml.rs
@@ -5,7 +5,7 @@ fn main()
     // generate from XML definition
     let raw_code = codespawn::from_xml("examples/sample.xml").unwrap();
     // generate from JSON definition
-    //let raw_code = codespawn::from_json("examples/sample.json").unwrap();
+    let raw_code = codespawn::from_json("examples/sample.json").unwrap();
 
     println!("\n*** Language specific configs (if defined):");
     for c in raw_code.configs.iter() {
@@ -15,8 +15,8 @@ fn main()
     println!("*** Structure of raw (unformatted) code data:\n{}", raw_code);
     println!("*** Structure of C++ code generated from raw code data:\n{}", raw_code.to_cpp());
     println!("*** Structure of Rust code generated from raw code data:\n{}", raw_code.to_rust());
-    println!("*** Generated C++ code:\n{}", raw_code.to_cpp().to_string());
-    println!("*** Generated Rust code:\n{}", raw_code.to_rust().to_string());
+    println!("*** Generated C++ code:\n\n{}", raw_code.to_cpp().to_string());
+    println!("*** Generated Rust code:\n\n{}", raw_code.to_rust().to_string());
 
     // save generated code to file
     let _ = raw_code.to_cpp().to_file("sample.cpp");
